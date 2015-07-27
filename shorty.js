1d4cd12328f8fa5d962445bb18d6b017ff44c7b7
oshai <oshai@gmail.com>
2015-07-27 13:41:25 +0300

    add more keys and refactor

--- shorty.js
+++ shorty.js
@@ -1,5 +1,7 @@
 if (Meteor.isClient) {
     var keys = [];
+    var text;
+    var desc;
 
     function removeAll(item) {
         var index = keys.indexOf(item);
@@ -7,111 +9,57 @@ if (Meteor.isClient) {
             keys.splice(index, 1);
             index = keys.indexOf(item);
         }
-    };
 
+    };
     function sortNumber(a,b) {
         return a - b;
     }
+    function setPropertiesIfKeysMatch(arrKeys, shortcut, description){
+        if (_.isEqual(keys, arrKeys)) {
+            desc = description;
+            text = shortcut;
+        }
+    }
     function display() {
         keys.sort(sortNumber);
-        var text =  keys;
-        var desc = 'no match';
-        if (_.isEqual(keys, [17,68])) {
-            desc = "delete line";
-            text = "cmd + Y";
-        }
-        if (_.isEqual(keys, [18,40])) {
-            desc = "move line down";
-            text = "alt + shift + down";
-        }
-        if (_.isEqual(keys, [18,38])) {
-            desc = "move line up";
-            text = "alt + shift + up";
-        }
-        if (_.isEqual(keys, [17,32])) {
-            desc = "auto complete";
-            text = "ctrl + (shift) + space";
-        }
-        if (_.isEqual(keys, [17,77])) {
-            desc = "maximize";
-            text = "cmd + shift + F12";
-        }
-        if (_.isEqual(keys, [16,17,84])) {
-            desc = "open type";
-            text = "cmd + N";
-        }
-        if (_.isEqual(keys, [17,49])) {
-            desc = "open quick fix (intention)";
-            text = "alt + enter";
-        }
-        if (_.isEqual(keys, [17,84])) {
-            desc = "type hierarchy";
-            text = "ctrl + H";
-        }
-        if (_.isEqual(keys, [17,81])) {
-            desc = "last edit location";
-            text = "ctrl + shift + backspace";
-        }
-        if (_.isEqual(keys, [17,73])) {
-            desc = "indent";
-            text = "cmd + alt + I";
-        }
-        if (_.isEqual(keys, [16,17,70])) {
-            desc = "format";
-            text = "cmd + alt + L";
-        }
-        if (_.isEqual(keys, [16,17,82])) {
-            desc = "open resource";
-            text = "cmd + shift + N";
-        }
-        if (_.isEqual(keys, [16,17,71])) {
-            desc = "references";
-            text = "alt + F7";
-        }
-        if (_.isEqual(keys, [17,18,40])) {
-            desc = "copy lines";
-            text = "cmd + D";
-        }
-        if (_.isEqual(keys, [17,18,38])) {
-            desc = "copy lines";
-            text = "cmd + D";
-        }
-        if (_.isEqual(keys, [17,72])) {
-            desc = "search";
-            text = "ctrl + shift + F";
-        }
-        if (_.isEqual(keys, [17,18,76])) {
-            desc = "extract variable";
-            text = "cmd + alt + V";
-        }
-        if (_.isEqual(keys, [16,18,84])) {
-            desc = "refactor";
-            text = "cmd + alt + shift + T";
-        }
-        if (_.isEqual(keys, [17,76])) {
-            desc = "goto line";
-            text = "cmd + G";
-        }
-        if (_.isEqual(keys, [17,79])) {
-            desc = "outline";
-            text = "cmd + F12";
-        }
-        if (_.isEqual(keys, [18,37])) {
-            desc = "previous editor";
-            text = "cmd + alt + left";
-        }
-        if (_.isEqual(keys, [68,91])) {
-            desc = "show desktop";
-            text = "F11";
-        }
+        text =  keys;
+        desc = 'no match';
+        setPropertiesIfKeysMatch([17,68],       "cmd + Y", "delete line");
+        setPropertiesIfKeysMatch([18,40],       "cmd + shift + down / alt + shift + down (no indentation)", "move line down");
+        setPropertiesIfKeysMatch([18,38],       "cmd + shift + up / alt + shift + up (no indentation)", "move line up");
+        setPropertiesIfKeysMatch([17,32],       "ctrl + (shift) + space", "auto complete");
+        setPropertiesIfKeysMatch([17,77],       "cmd + shift + F12", "maximize");
+        setPropertiesIfKeysMatch([16,17,84],    "cmd + N", "open type");
+        setPropertiesIfKeysMatch([17,49],       "alt + enter", "open quick fix (intention)");
+        setPropertiesIfKeysMatch([17,84],       "ctrl + H", "type hierarchy");
+        setPropertiesIfKeysMatch([17,81],       "cmd + shift + backspace", "last edit location");
+        setPropertiesIfKeysMatch([17,73],       "cmd + alt + I", "indent");
+        setPropertiesIfKeysMatch([16,17,70],    "cmd + alt + L", "format");
+        setPropertiesIfKeysMatch([16,17,79],    "cmd + alt + O", "organize imports");
+        setPropertiesIfKeysMatch([16,17,82],    "cmd + shift + N", "open resource");
+        setPropertiesIfKeysMatch([16,17,71],    "alt + F7", "references");
+        setPropertiesIfKeysMatch([17,18,40],    "cmd + D", "copy lines");
+        setPropertiesIfKeysMatch([17,18,82],    "shift + F6", "rename");
+        setPropertiesIfKeysMatch([17,72],       "ctrl + shift + F", "search");
+        setPropertiesIfKeysMatch([17,18,76],    "cmd + alt + V", "extract variable");
+        setPropertiesIfKeysMatch([17,18,73],    "cmd + alt + N", "inline variable");
+        setPropertiesIfKeysMatch([16,18,84],    "cmd + alt + shift + T", "refactor");
+        setPropertiesIfKeysMatch([17,76],       "cmd + G", "goto line");
+        setPropertiesIfKeysMatch([17,79],       "cmd + F12", "outline");
+        setPropertiesIfKeysMatch([18,37],       "cmd + alt + left", "previous editor");
+        setPropertiesIfKeysMatch([68,91],       "F11", "show desktop");
+        setPropertiesIfKeysMatch([16,17,73],    "ctrl + alt + F8", "evaluate");
+        setPropertiesIfKeysMatch([17,18,37],    "cmd + shift + F8", "decrease selection");
+        setPropertiesIfKeysMatch([17,18,39],    "cmd + W", "increase selection");
         $("#theInput").text(text);
         $("#theDescription").text(desc);
     };
 
     onkeydown = function(e){
         e.preventDefault();
-        if (keys.indexOf(e.keyCode) === -1)
+        if (keys.indexOf(e.keyCode) === -1) {
             keys.push(e.keyCode);
+        }
         display();
     };
     onkeyup = function(e){
