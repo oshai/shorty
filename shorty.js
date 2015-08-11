85dcc4ae922c6f2cdf071c63955bfd6e063bc8fd
oshai <oshai@gmail.com>
2015-08-11 08:54:35 +0300

    improved mapping

--- shorty.js
+++ shorty.js
@@ -2,7 +2,39 @@ if (Meteor.isClient) {
     var keys = [];
     var text;
     var desc;
+    var mapping = [];
 
+    putInMap([17,68],       "⌘ + Y", "delete line");
+    putInMap([18,40],       "⌘ + ⇧ + down | ⌥ + ⇧ + down (no indentation)", "move line down");
+    putInMap([18,38],       "⌘ + ⇧ + up | ⌥ + ⇧ + up (no indentation)", "move line up");
+    putInMap([17,32],       "^ + (⇧) + space | ⌘ + J (templates)", "auto complete");
+    putInMap([17,77],       "⌘ + ⇧ + F12", "maximize");
+    putInMap([16,17,84],    "⌘ + N", "open type");
+    putInMap([17,49],       "⌥ + enter", "open quick fix (intention)");
+    putInMap([17,84],       "^ + H", "type hierarchy");
+    putInMap([17,81],       "⌘ + ⇧ + backspace", "last edit location");
+    putInMap([17,73],       "⌘ + ⌥ + I", "indent");
+    putInMap([16,17,70],    "⌘ + ⌥ + L", "format");
+    putInMap([16,17,79],    "⌘ + ⌥ + O", "organize imports");
+    putInMap([16,17,82],    "⌘ + ⇧ + N", "open resource");
+    putInMap([16,17,71],    "⌥ + F7", "references");
+    putInMap([17,18,40],    "⌘ + D", "copy lines");
+    putInMap([17,18,82],    "⇧ + F6", "rename");
+    putInMap([17,72],       "^ + ⇧ + F", "search");
+    putInMap([17,18,76],    "⌘ + ⌥ + V", "extract variable");
+    putInMap([17,18,73],    "⌘ + ⌥ + N", "inline variable");
+    putInMap([16,18,84],    "⌘ + ⌥ + ⇧ + T", "refactor");
+    putInMap([17,76],       "⌘ + G", "goto line");
+    putInMap([17,79],       "⌘ + F12", "outline");
+    putInMap([18,37],       "⌘ + ⌥ + left", "previous editor");
+    putInMap([68,91],       "F11", "show desktop");
+    putInMap([16,17,73],    "^ + ⌥ + F8", "evaluate");
+    putInMap([17,18,37],    "⌘ + ⇧ + F8", "decrease selection");
+    putInMap([17,37,91],    "⇧ + F7", "smart step into");
+
+    Template.body.helpers({
+        mapping : mapping
+    });
     function removeAll(item) {
         var index = keys.indexOf(item);
         while (index !== -1) {
@@ -14,45 +46,22 @@ if (Meteor.isClient) {
     function sortNumber(a,b) {
         return a - b;
     }
-    function setPropertiesIfKeysMatch(arrKeys, shortcut, description){
-        if (_.isEqual(keys, arrKeys)) {
-            desc = description;
-            text = shortcut;
+    function getFromMap(arrKeys){
+        for (var i = 0; i < mapping.length; i++) {
+            if (_.isEqual(arrKeys, mapping[i].pressed)) {
+                return mapping[i];
+            }
         }
+        return {value: arrKeys, description: 'no match'}
+    }
+    function putInMap(arrKeys, shortcut, description){
+        mapping.push({pressed: arrKeys, value: shortcut, description: description})
     }
     function display() {
         keys.sort(sortNumber);
-        text =  keys;
-        desc = 'no match';
-        setPropertiesIfKeysMatch([17,68],       "cmd + Y", "delete line");
-        setPropertiesIfKeysMatch([18,40],       "cmd + shift + down / alt + shift + down (no indentation)", "move line down");
-        setPropertiesIfKeysMatch([18,38],       "cmd + shift + up / alt + shift + up (no indentation)", "move line up");
-        setPropertiesIfKeysMatch([17,32],       "ctrl + (shift) + space", "auto complete");
-        setPropertiesIfKeysMatch([17,77],       "cmd + shift + F12", "maximize");
-        setPropertiesIfKeysMatch([16,17,84],    "cmd + N", "open type");
-        setPropertiesIfKeysMatch([17,49],       "alt + enter", "open quick fix (intention)");
-        setPropertiesIfKeysMatch([17,84],       "ctrl + H", "type hierarchy");
-        setPropertiesIfKeysMatch([17,81],       "cmd + shift + backspace", "last edit location");
-        setPropertiesIfKeysMatch([17,73],       "cmd + alt + I", "indent");
-        setPropertiesIfKeysMatch([16,17,70],    "cmd + alt + L", "format");
-        setPropertiesIfKeysMatch([16,17,79],    "cmd + alt + O", "organize imports");
-        setPropertiesIfKeysMatch([16,17,82],    "cmd + shift + N", "open resource");
-        setPropertiesIfKeysMatch([16,17,71],    "alt + F7", "references");
-        setPropertiesIfKeysMatch([17,18,40],    "cmd + D", "copy lines");
-        setPropertiesIfKeysMatch([17,18,82],    "shift + F6", "rename");
-        setPropertiesIfKeysMatch([17,72],       "ctrl + shift + F", "search");
-        setPropertiesIfKeysMatch([17,18,76],    "cmd + alt + V", "extract variable");
-        setPropertiesIfKeysMatch([17,18,73],    "cmd + alt + N", "inline variable");
-        setPropertiesIfKeysMatch([16,18,84],    "cmd + alt + shift + T", "refactor");
-        setPropertiesIfKeysMatch([17,76],       "cmd + G", "goto line");
-        setPropertiesIfKeysMatch([17,79],       "cmd + F12", "outline");
-        setPropertiesIfKeysMatch([18,37],       "cmd + alt + left", "previous editor");
-        setPropertiesIfKeysMatch([68,91],       "F11", "show desktop");
-        setPropertiesIfKeysMatch([16,17,73],    "ctrl + alt + F8", "evaluate");
-        setPropertiesIfKeysMatch([17,18,37],    "cmd + shift + F8", "decrease selection");
-        setPropertiesIfKeysMatch([17,18,39],    "cmd + W", "increase selection");
-        $("#theInput").text(text);
-        $("#theDescription").text(desc);
+        var ret = getFromMap(keys);
+        $("#theInput").text(ret.value);
+        $("#theDescription").text(ret.description);
     };
 
     onkeydown = function(e){
